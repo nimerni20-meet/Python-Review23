d8d3588911bc4f44c3e949a2465292eb4b414c76
nimerni20-meet <139688587+nimerni20-meet@users.noreply.github.com>
2023-07-17 12:09:44 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,15 @@
+# def test_func(name):
+# 	print("hello " + name)
+
+# test_func("Nimer")
+
+
+
+
+def create_youtube_video(title,description):
+	tdf= {"title": "","description": "","likes":0,"dislikes":0,"comments":""}
+	return tdf
+
+def like():
+	youtube video= {}
+return youtube_video
